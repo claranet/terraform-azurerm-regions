4a300ed28eb134ab6492fa548ae7533945172bd1
Jérôme Respaut <jerome.respaut@fr.clara.net>
2021-01-05 15:08:55 +0100

    AZ-419: Add Azure CLI region name  and also update region list

--- regions.tf
+++ regions.tf
@@ -4,6 +4,7 @@ locals {
   regions = {
     us-west          = "West US"
     us-west-2        = "West US 2"
+    us-central       = "Central US"
     us-west-central  = "West Central US"
     us-south-central = "South Central US"
     us-north-central = "North Central US"
@@ -12,12 +13,14 @@ locals {
     can-central      = "Canada Central"
     can-east         = "Canada East"
     bra-south        = "Brasil South"
+    bra-south-east   = "Brazil Southeast"
     uk-south         = "UK South"
     uk-west          = "UK West"
     eu-north         = "North Europe"
     eu-west          = "West Europe"
     fr-central       = "France Central"
     fr-south         = "France South"
+    ger-north        = "Germany North"
     ger-north-east   = "Germany Northeast"
     ger-central      = "Germany Central"
     ger-west-central = "Germany West Central"
@@ -32,8 +35,10 @@ locals {
     ind-south        = "South India"
     asia-south-east  = "Southeast Asia"
     asia-east        = "East Asia"
-    ch-north         = "China North"
     ch-east          = "China East"
+    cn-east-2        = "China East 2"
+    ch-north         = "China North"
+    cn-north-2       = "China North 2"
     kor-central      = "Korea Central"
     kor-south        = "Korea South"
     jap-east         = "Japan East"
@@ -44,6 +49,19 @@ locals {
     aus-south-east   = "Australia Southeast"
     uae-central      = "UAE Central" # United Arab Emirates
     uae-north        = "UAE North"   # United Arab Emirates
+
+    # Global/continental zones
+    "asia"    = "Asia"
+    "asia-pa" = "Asia Pacific"
+    "aus"     = "Australia"
+    "bra"     = "Brazil"
+    "can"     = "Canada"
+    "eu"      = "Europe"
+    "global"  = "Global"
+    "ind"     = "India"
+    "jap"     = "Japan"
+    "uk"      = "United Kingdom"
+    "us"      = "United States"
   }
 
   short_names = {
@@ -58,6 +76,7 @@ locals {
     "can-east"         = "cae"
     "can-central"      = "cac"
     "bra-south"        = "brs"
+    "bra-south-east"   = "brse" # Brazil Southeast
     "eu-north"         = "eun"
     "eu-west"          = "euw"
     "fr-central"       = "frc"
@@ -75,7 +94,7 @@ locals {
     "asia-south-east"  = "ase"
     "asia-east"        = "ae"
     "aus-east"         = "aue"
-    "aus-south"        = "ause"
+    "aus-south"        = "aus"
     "aus-central"      = "auc"
     "aus-central-2"    = "auc2"
     "cn-east"          = "cne"
@@ -93,6 +112,80 @@ locals {
     "saf-north"        = "san"
     "uae-central"      = "uaec"
     "uae-north"        = "uaen"
+
+    # Global/continental zones
+    "asia"    = "asia"   # Asia
+    "asia-pa" = "asiapa" # Asia Pacific
+    "aus"     = "aus"    # Australia
+    "bra"     = "bra"    # Brazil
+    "can"     = "can"    # Canada
+    "eu"      = "eu"     # Europe
+    "global"  = "glob"   # Global
+    "ind"     = "ind"    # India
+    "jap"     = "jap"    # Japan
+    "uk"      = "uk"     # United Kingdom
+    "us"      = "us"     # United States
+  }
+
+  cli_names = {
+    "us-east"          = "eastus"             # East US
+    "us-east-2"        = "eastus2"            # East US 2
+    "us-south-central" = "southcentralus"     # South Central US
+    "us-west-2"        = "westus2"            # West US 2
+    "aus-east"         = "australiaeast"      # Australia East
+    "asia-south-east"  = "southeastasia"      # Southeast Asia
+    "eu-north"         = "northeurope"        # North Europe
+    "uk-south"         = "uksouth"            # UK South
+    "eu-west"          = "westeurope"         # West Europe
+    "us-central"       = "centralus"          # Central US
+    "us-north-central" = "northcentralus"     # North Central US
+    "us-west"          = "westus"             # West US
+    "saf-north"        = "southafricanorth"   # South Africa North
+    "ind-central"      = "centralindia"       # Central India
+    "asia-east"        = "eastasia"           # East Asia
+    "jap-east"         = "japaneast"          # Japan East
+    "kor-central"      = "koreacentral"       # Korea Central
+    "can-central"      = "canadacentral"      # Canada Central
+    "fr-central"       = "francecentral"      # France Central
+    "ger-west-central" = "germanywestcentral" # Germany West Central
+    "norw-east"        = "norwayeast"         # Norway East
+    "swz-north"        = "switzerlandnorth"   # Switzerland North
+    "uae-north"        = "uaenorth"           # UAE North
+    "bra-south"        = "brazilsouth"        # Brazil South
+    "asia"             = "asia"               # Asia
+    "asia-pa"          = "asiapacific"        # Asia Pacific
+    "aus"              = "australia"          # Australia
+    "bra"              = "brazil"             # Brazil
+    "can"              = "canada"             # Canada
+    "eu"               = "europe"             # Europe
+    "global"           = "global"             # Global
+    "ind"              = "india"              # India
+    "jap"              = "japan"              # Japan
+    "uk"               = "uk"                 # United Kingdom
+    "us"               = "unitedstates"       # United States
+    "us-west-central"  = "westcentralus"      # West Central US
+    "saf-west"         = "southafricawest"    # South Africa West
+    "aus-central"      = "australiacentral"   # Australia Central
+    "aus-central-2"    = "australiacentral2"  # Australia Central 2
+    "aus-south-east"   = "australiasoutheast" # Australia Southeast
+    "jap-west"         = "japanwest"          # Japan West
+    "kor-south"        = "koreasouth"         # Korea South
+    "ind-south"        = "southindia"         # South India
+    "ind-west"         = "westindia"          # West India
+    "can-east"         = "canadaeast"         # Canada East
+    "fr-south"         = "francesouth"        # France South
+    "ger-north"        = "germanynorth"       # Germany North
+    "norw-west"        = "norwaywest"         # Norway West
+    "swz-west"         = "switzerlandwest"    # Switzerland West
+    "uk-west"          = "ukwest"             # UK West
+    "uae-central"      = "uaecentral"         # UAE Central
+    "bra-south-east"   = "brazilsoutheast"    # Brazil Southeast
+    "ger-north-east"   = "germanynortheast"   # "Germany Northeast"
+    "ger-central"      = "germanycentral"     # "Germany Central"
+
+    "ch-north"   = "chinanorth"  # "China North"
+    "ch-east"    = "chinaeast"   # "China East"
+    "cn-east-2"  = "chinaeast2"  # "China East 2"
+    "cn-north-2" = "chinanorth2" # "China North 2"
   }
 }
-
